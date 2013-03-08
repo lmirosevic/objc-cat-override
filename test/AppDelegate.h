e02dc094482117aa4ea46a5649a71cc55dbb30f9
Luka Mirosevic <luka@goonbee.com>
2013-03-08 12:52:13 +0100

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Luka Mirosevic on 08/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  test
+//
+//  Created by Luka Mirosevic on 08/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
